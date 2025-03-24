06e10fc045047ccf593a7544ca1ba66639db2843
dhyey <d1373@protonmail.com>
2025-03-24 20:24:28 +0530

    basic neovim setup
    making it into the best setup available

---
+++ nvim/.config/nvim/lua/ds/options.lua
@@ -0,0 +1,32 @@
+vim.opt.clipboard:append('unnamedplus')
+vim.g.clipboard = {
+  name = 'win32yank-wsl',
+  copy = {
+    ['+'] = 'win32yank.exe -i --crlf',
+    ['*'] = 'win32yank.exe -i --crlf',
+  },
+  paste = {
+    ['+'] = 'win32yank.exe -o --lf',
+    ['*'] = 'win32yank.exe -o --lf',
+  },
+  cache_enabled = 0,
+}
+
+-- Editor settings
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.path:append('**') -- Recursive path search
+vim.opt.wildmenu = true
+vim.opt.incsearch = true
+vim.opt.hidden = true
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.g.have_nerd_font = false
+vim.opt.showmode = false
+vim.opt.undofile = true
---
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  opts = {
+    -- fill any relevant options here
+  },
+  config = function()
+      require('neo-tree').setup({
+  close_if_last_window = true,
+})
+    vim.keymap.set('', '<M-b>', ':Neotree toggle<CR>', { silent = true })
+  end
+}
