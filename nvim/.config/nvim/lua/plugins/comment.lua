7d5a7457cc936f08d5f9fb466f8975601c586454
dhyey <d1373@protonmail.com>
2025-04-14 22:57:10 +0530

    working in visual mode for nerd commenter

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -4,5 +4,6 @@ return {
 	priority = 1000,
 	config = function()
 		vim.keymap.set("n", "<M-/>", "<Plug>NERDCommenterToggle", { desc = "Toggle comment" })
+		vim.keymap.set("v", "<M-/>", "<Plug>NERDCommenterToggle", { desc = "Toggle comment" })
 	end,
 }
