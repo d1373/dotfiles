60f66bb0bdb0b8afe9e755a1b0bf9ac073582ee7
d1373 <d1373@protonmail.com>
2025-07-07 15:30:25 +0530

    change colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,12 @@
 return {
-	"sainnhe/gruvbox-material",
+	"sainnhe/sonokai",
 	lazy = false,
 	priority = 1000,
 	config = function()
 		-- Optionally configure and load the colorscheme
 		-- directly inside the plugin declaration.
-		vim.g.gruvbox_material_enable_italic = true
-		vim.g.gruvbox_material_transparent_background = 1
-		vim.cmd [[let g:gruvbox_material_colors_override = {'fg0': ['#c1c1c1','223']}]]
-		vim.cmd.colorscheme("gruvbox-material")
+		vim.g.sonokai_enable_italic = true
+		vim.g.sonokai_transparent_background = 1
+		vim.cmd.colorscheme("sonokai")
 	end,
 }
