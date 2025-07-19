fe6684d2d9eeb841cd00605855f7afadde0b5f31
d1373 <d1373@protonmail.com>
2025-07-19 12:35:53 +0530

    added smear cursor to neovim
    This makes it really better to read

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -78,4 +78,27 @@ return {
 			require("mini.ai").setup()
 		end,
 	},
+	{
+		"sphamba/smear-cursor.nvim",
+		opts = { -- Default  Range
+			stiffness = 0.8, -- 0.6      [0, 1]
+			trailing_stiffness = 0.5, -- 0.4      [0, 1]
+			stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+			trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+			damping = 0.8, -- 0.65     [0, 1]
+			damping_insert_mode = 0.8, -- 0.7      [0, 1]
+			distance_stop_animating = 0.5, -- 0.1      > 0
+		},
+		--config = function()
+		--require("smear-cursor").setup({
+		--stiffness = 0.8,
+		--trailing_stiffness = 0.5,
+		--stiffness_insert_mode = 0.7,
+		--trailing_stiffness_insert_mode = 0.7,
+		--damping = 0.8,
+		--damping_insert_mode = 0.8,
+		--distance_stop_animating = 0.5,
+		--})
+		--end,
+	},
 }
